2e72c693d7b2ad0d440d04c2262e28d0e4c89c03
Noklent Fardian <noklent320@gmail.com>
2022-01-04 13:11:26 +0700

    Change Title

--- resources/views/pembayaran/base.blade.php
+++ resources/views/pembayaran/base.blade.php
@@ -3,7 +3,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>Bayar Listrik | Level</title>
+    <title>Bayar Listrik |</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
